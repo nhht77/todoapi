c479795e55864f134c88a7d6af8c48aadd897c2c
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-11-20 08:22:39 +0200

    Refactored Code

--- routes/todos.js
+++ routes/todos.js
@@ -1,35 +1,15 @@
 const express = require('express'),
       router  = express.Router(),
-      db      = require('../models');
+      db      = require('../models'),
+      helpers = require("../helpers/todos");
 
-router.get('/', (req, res) => {
-      db.Todo.find()
-      .then(todos => res.json(todos))
-      .catch( err => res.send(err));
-});
-
-router.post("/", (req,res) => {
-      db.Todo.create(req.body)
-      .then( newTodo => res.status(201).json(newTodo))
-      .catch( err => res.send(err));
-})
-
-router.get("/:id", (req, res) => {
-      db.Todo.findById(req.params.id)
-      .then( Todo => res.json(Todo) )
-      .catch( err => res.send(err) );
-})
-
-router.put("/:id", (req, res) => {
-      db.Todo.findByIdAndUpdate({_id: req.params.id}, req.body )
-      .then( Todo => res.json(Todo) )
-      .catch( err => res.send(err) );
-})
-
-router.delete("/:id", (req, res) => {
-      db.Todo.findByIdAndDelete({_id: req.params.id})
-      .then( res.json({message: "It is deleted"}))
-      .catch( err => res.send(err));
-})
-
+router.route('/')
+      .get(helpers.getTodos)
+      .post(helpers.createTodo)
+      
+router.route('/:todoId')
+      .get(helpers.getTodo)
+      .put(helpers.updateTodo)
+      .delete(helpers.deleteTodo)
+      
 module.exports = router;
