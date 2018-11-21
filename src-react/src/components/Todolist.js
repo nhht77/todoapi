ec219ea6509af63f75fd8b9858c849178871a789
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-11-21 16:11:36 +0200

    adding newTodo to state and DB

--- src-react/src/components/Todolist.js
+++ src-react/src/components/Todolist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 
 const apiURL = "api/todos";
 
@@ -9,6 +10,8 @@ class Todolist extends Component {
         this.state={
             todos:[]
         }
+
+    this.addTodo = this.addTodo.bind(this);
     }
 
     componentWillMount(){
@@ -35,6 +38,34 @@ class Todolist extends Component {
         .then (todos => this.setState({todos}));
     }
 
+    addTodo = val => { 
+        fetch(apiURL, {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type':'Application/json'
+            }),
+            body: JSON.stringify({name: val})
+        })
+        .then( res => {
+            if(!res.ok){
+                if(res.status >= 400 && res.status < 500){
+                    return res.json().then( data => 
+                        { 
+                            let err = {errorMessage: data.message} 
+                            throw err;
+                        })
+                } else {
+                    let err = {errorMessage: 'Server is offline, please try again!'}
+                    throw err;
+                }
+            }
+            return res.json();
+        })
+        .then( newTodo => {
+            this.setState({todos: [...this.state.todos, newTodo] })
+        })
+    }
+
   render() {
     const todos = this.state.todos.map( t => {
         return <TodoItem key={t._id} {...t}/>
@@ -42,6 +73,7 @@ class Todolist extends Component {
     return (
       <div>
         <h1>Todo List</h1>
+        <TodoForm addTodo={this.addTodo}/>
         <ul>{todos}</ul>
       </div>
     )
